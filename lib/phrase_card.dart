5bab4a8e916d8a13273e1623e492d4707ddada1e
Lina Gurevich <gurlina@Linas-MacBook-Pro.local>
2020-10-26 20:49:16 -0700

    added Firebase phrasebook support

---
+++ lib/phrase_card.dart
@@ -0,0 +1,136 @@
+import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:lipspeak/model/phrase_book.dart';
+
+class PhraseCard extends StatelessWidget {
+  final QuerySnapshot snapshot;
+  final int index;
+  const PhraseCard({Key key, this.snapshot, this.index}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    var snapshotData = snapshot.docs[index].data();
+    var docId = snapshot.docs[index].id;
+    var collectionReference =
+        FirebaseFirestore.instance.collection(PhraseFS.fbCollectionName);
+
+    TextEditingController queryInputController =
+        TextEditingController(text: snapshotData[PhraseFS.fbQueryFieldName]);
+    TextEditingController textInputController =
+        TextEditingController(text: snapshotData[PhraseFS.fbTextFieldName]);
+
+    return Column(
+      children: [
+        Container(
+          height: 130,
+          child: Card(
+            elevation: 9,
+            child: Column(
+              children: [
+                ListTile(
+                  title: Text(
+                    "${snapshotData["query"]}",
+                    style: TextStyle(fontWeight: FontWeight.w400, fontSize: 20),
+                  ),
+                  subtitle: Text(
+                    snapshotData["text"],
+                    style: TextStyle(fontSize: 18),
+                  ),
+                  leading: IconButton(
+                      icon: Icon(
+                        Icons.mic,
+                        size: 40,
+                      ),
+                      onPressed: () {}),
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  children: [
+                    IconButton(
+                        icon: Icon(
+                          Icons.edit,
+                          size: 25,
+                        ),
+                        onPressed: () async {
+                          await showDialog(
+                              context: context,
+                              child: AlertDialog(
+                                contentPadding: EdgeInsets.all(10),
+                                content: Column(
+                                  children: [
+                                    Text("Please fill out the form to update."),
+                                    Expanded(
+                                        child: TextField(
+                                      autofocus: true,
+                                      autocorrect: true,
+                                      decoration: InputDecoration(
+                                          labelText: "Keyword(s):"),
+                                      controller: queryInputController,
+                                    )),
+                                    Expanded(
+                                        child: TextField(
+                                      autofocus: true,
+                                      autocorrect: true,
+                                      decoration: InputDecoration(
+                                          labelText: "Generated phrase:"),
+                                      controller: textInputController,
+                                    )),
+                                  ],
+                                ),
+                                actions: [
+                                  FlatButton(
+                                      onPressed: () {
+                                        // nameInputController.clear();
+                                        // titleInputController.clear();
+                                        // descriptionInputController.clear();
+
+                                        Navigator.pop(context);
+                                      },
+                                      child: Text("Cancel")),
+                                  FlatButton(
+                                      onPressed: () {
+                                        if (queryInputController
+                                                .text.isNotEmpty &&
+                                            textInputController
+                                                .text.isNotEmpty) {
+                                          FirebaseFirestore.instance
+                                              .collection(
+                                                  PhraseFS.fbCollectionName)
+                                              .doc(docId)
+                                              .update({
+                                            PhraseFS.fbQueryFieldName:
+                                                queryInputController.text,
+                                            PhraseFS.fbTextFieldName:
+                                                textInputController.text
+                                          }).then((response) {
+                                            Navigator.pop(context);
+                                          });
+                                        }
+                                      },
+                                      child: Text("Update"))
+                                ],
+                              ));
+                        }),
+                    SizedBox(
+                      height: 19,
+                    ),
+                    IconButton(
+                        icon: Icon(
+                          Icons.delete_forever_outlined,
+                          size: 25,
+                        ),
+                        onPressed: () async {
+                          await collectionReference.doc(docId).delete();
+                        })
+                  ],
+                )
+              ],
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+
+  void _playText() {}
+}
---
+++ lib/phrasebook.dart
@@ -0,0 +1,121 @@
+import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:lipspeak/model/phrase_book.dart';
+import 'package:lipspeak/phrase_card.dart';
+
+class PhraseBook extends StatefulWidget {
+  PhraseBook({Key key}) : super(key: key);
+
+  @override
+  _PhraseBookState createState() => _PhraseBookState();
+}
+
+class _PhraseBookState extends State<PhraseBook> {
+  var firestoreDb = FirebaseFirestore.instance
+      .collection(PhraseFS.fbCollectionName)
+      .snapshots();
+  TextEditingController queryInputController;
+  TextEditingController textInputController;
+
+  @override
+  void initState() {
+    super.initState();
+
+    queryInputController = new TextEditingController();
+    textInputController = new TextEditingController();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Phrasebook"),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          _showDialog(context);
+        },
+        child: Icon(Icons.add),
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
+      body: StreamBuilder(
+          stream: firestoreDb,
+          builder: (context, snapshot) {
+            if (!snapshot.hasData) {
+              return CircularProgressIndicator();
+            } else {
+              return ListView.builder(
+                  itemCount: snapshot.data.documents.length,
+                  itemBuilder: (context, int index) {
+                    return PhraseCard(snapshot: snapshot.data, index: index);
+                    //return Text(snapshot.data.documents[index]['text']);
+                  });
+            }
+          }),
+    );
+  }
+
+  _showDialog(BuildContext context) async {
+    await showDialog(
+        context: context,
+        child: AlertDialog(
+          contentPadding: EdgeInsets.all(10),
+          content: Column(
+            children: [
+              Text("Add new entry to the phrasebook:"),
+              Expanded(
+                  child: TextField(
+                autofocus: true,
+                autocorrect: true,
+                decoration: InputDecoration(
+                    icon: Icon(Icons.camera_front_outlined),
+                    labelText: "Keyword(s)",
+                    helperText: "separate multiple keywords with spaces",
+                    helperMaxLines: 3),
+                controller: queryInputController,
+              )),
+              Expanded(
+                  child: TextField(
+                autofocus: true,
+                autocorrect: true,
+                decoration: InputDecoration(
+                  icon: Icon(Icons.mic_outlined),
+                  labelText: "Generated phrase",
+                ),
+                controller: textInputController,
+              )),
+            ],
+          ),
+          actions: [
+            FlatButton(
+                onPressed: () {
+                  queryInputController.clear();
+                  textInputController.clear();
+
+                  Navigator.pop(context);
+                },
+                child: Text("Cancel")),
+            FlatButton(
+                onPressed: () {
+                  if (queryInputController.text.isNotEmpty &&
+                      textInputController.text.isNotEmpty) {
+                    FirebaseFirestore.instance
+                        .collection(PhraseFS.fbCollectionName)
+                        .add({
+                      PhraseFS.fbQueryFieldName: queryInputController.text,
+                      PhraseFS.fbTextFieldName: textInputController.text,
+                    }).then((response) {
+                      //print(response.id);
+
+                      Navigator.pop(context);
+
+                      queryInputController.clear();
+                      textInputController.clear();
+                    }).catchError((error) => print(error));
+                  }
+                },
+                child: Text("Save"))
+          ],
+        ));
+  }
+}
